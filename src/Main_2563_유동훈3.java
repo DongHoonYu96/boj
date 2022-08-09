e4b72d6ee1c517b5ceeeac6aea18008f8a3d8d7d
DONGHOONYU96 <sbna154@gmail.com>
2022-08-09 17:54:35 +0900

    0809

---
+++ src/Main_2563_유동훈3.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main_2563_유동훈3 {
+
+	static int[][] map=new int[101][101];
+	static int count;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        
+        
+        
+        
+        for(int T=0;T<n;++T)
+        {
+        	st = new StringTokenizer(br.readLine());
+        	int x=Integer.parseInt(st.nextToken());
+        	int y=Integer.parseInt(st.nextToken());
+        	
+        	for(int i=x;i<x+10;++i)
+        	{
+        		for(int j=y;j<y+10;++j)
+        		{
+        			
+        		}
+        	}
+        	
+        	
+        	
+        }
+        
+        
+        
+        
+        
+       
+    }
+}    
