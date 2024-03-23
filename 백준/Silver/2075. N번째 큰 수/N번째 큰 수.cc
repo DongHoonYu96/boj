f8d35a92eea4e093dd8e892accad6f52379b710e
DongHoonYu96 <50190387+DongHoonYu96@users.noreply.github.com>
2024-03-24 03:47:38 +0900

    [Silver II] Title: N번째 큰 수, Time: 364 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,24 +1,24 @@
-// Authored by : heheHwang
-// Co-authored by : -
-// http://boj.kr/45e1c40b488544b89ca5d9c966a6cdc0
 #include <bits/stdc++.h>
+
 using namespace std;
+typedef long long ll;
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+ll ret,n,tmp;
+priority_queue<ll,vector<ll>,greater<ll>> pq;
 
-  int N, num;
-  priority_queue<int, vector<int>, greater<>> pq;
-  cin >> N;
-  for (int i = 0; i < N * N; i++) {
-    cin >> num;
-    pq.push(num);
-    if (N < (int)pq.size()) pq.pop();
-  }
-  cout << pq.top() << '\n';
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n*n; ++i) {
+		cin >> tmp;
+		pq.push(tmp);
+		if (n < pq.size()) pq.pop();
+	}
+
+	cout << pq.top();
+
+	
+	return 0;
 }
-
-/*
-문제의 메모리 제한을 고려해 우선순위 큐에 상위 N개의 값만 가지고 있는다.
-*/
