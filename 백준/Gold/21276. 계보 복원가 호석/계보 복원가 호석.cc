a8c054535fd1ec9782c1feebe0e21138a56bf6a7
DongHoonYu96 <50190387+DongHoonYu96@users.noreply.github.com>
2024-05-05 16:05:53 +0900

    [Gold III] Title: 계보 복원가 호석, Time: 256 ms, Memory: 5084 KB, Score: 19 point -BaekjoonHub

--- 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.cc
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.cc
@@ -1,72 +1,56 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-
-map<string, int> indeg,outdeg;
-int n,m;
-map<string, vector<string>> adj;
+map<string, int> idx;
+vector<vector<int>> G(1005);
 vector<string> v;
-
+vector<vector<int>> children(1005);
+int N, M;
+int indegree[1005];
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    
-    cin >> n;
-    for (int i = 0; i < n; ++i) {
-        string s;
-        cin >> s;
-        v.push_back(s); //사람명단
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+    cin >> N;
+    for(int i=0;i<N;++i) {
+        string s; cin >> s;
+        v.push_back(s);
     }
-    cin >> m;
-    for (int i = 0; i < m; ++i) {
-        string s1, s2;
-        cin >> s1 >> s2;
-        adj[s2].push_back(s1); //상도의 자식 : 호석
-        adj[s1]; //자식없어도 초기화는 해줘야함! -> 0명일때 출력
-        indeg[s1]++; //상도 -> 호석, 자식한테 화살표 들어옴
+    sort(v.begin(), v.end());
+    for(int i=0;i<N;++i) idx[v[i]] = i;
+    cin >> M;
+    for(int i=0;i<M;++i) {
+        string a, b;
+        cin >> a >> b;
+        int u = idx[a];
+        int v = idx[b];
+        G[v].push_back(u);
+        indegree[u]++;
     }
-
-    //indeg가 0 -> 조상
-    vector<string> josang;
-    for (auto item : adj) {
-        if (indeg[item.first] == 0) {
-            josang.push_back(item.first);
+    queue<int> q;
+    vector<int> super_father;
+    for(int i=0;i<N;++i) {
+        if(indegree[i] == 0) {
+            super_father.push_back(i);
+            q.push(i);
         }
     }
-
-    sort(josang.begin(), josang.end());
-
-    queue<string> q;
-    cout << josang.size()<<"\n";
-    for (auto s : josang) {
-        cout << s << " ";
-        q.push(s);
-    }cout << "\n";
-
-    map<string, vector<string>> res; //key의 직속자식들
-    //초기값 : indeg가 0인 조상들
-    while (q.size()) {
-        auto cur = q.front(); q.pop();
-        res[cur]; //맵 초기 할당
-        //모든 후손에대해
-        //나와 연결을 끊으면 indeg가 0이다(현재1) -> 직속자식이다!
-        for (auto s : adj[cur]) {
-            if (indeg[s] == 1) {
-                res[cur].push_back(s);
-                q.push(s);
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for(int nxt : G[cur]) {
+            indegree[nxt]--;
+            if(indegree[nxt] == 0) {
+                children[cur].push_back(nxt);
+                q.push(nxt);
             }
-            indeg[s]--; //연결끊기
         }
     }
-
-    for (auto item : res) {
-        cout << item.first << " " << item.second.size() << " ";
-        sort(item.second.begin(), item.second.end());
-        for (auto child : item.second) {
-            cout << child << " ";
-        }cout << "\n";
+    cout << super_father.size() << '\n';
+    for(int i : super_father) cout << v[i] << ' ';
+    cout << '\n';
+    for(int i=0;i<N;++i) {
+        cout << v[i] << ' ' << children[i].size() << ' ';
+        sort(children[i].begin(), children[i].end());
+        for(int j : children[i]) cout << v[j] << ' ';
+        cout << '\n';
     }
     return 0;
-
 }
