078cd712d3f27a698f16a33511e1132be888c098
DongHoonYu96 <50190387+DongHoonYu96@users.noreply.github.com>
2024-05-08 07:13:49 +0900

    [level 3] Title: 주사위 고르기, Time: 104.53 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.cpp
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.cpp
@@ -1,85 +1,92 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-vector<int> arrA,arrB;
+int n,arr[11],vis[11],mx;
+unordered_set<int> s;
 vector<vector<int>> dice;
-int n;
-int maxA, vis[6];
+vector<int> arrA,arrB;
+vector<int> answer;
 
-void go(int depth, int val, vector<int> &idxs){
-    cout<<depth<<" "<<val<<"\n";
-    if(depth==n/2){
-        arrA.push_back(val);
-        return;
+//a의 각원소에대해
+//b보다 큰 갯수 == A의 승리갯수
+int go2(vector<int>& a, vector<int>& b){
+    int ret=0;
+    sort(b.begin(),b.end()); //이분탐색은 정렬후 사용!
+    for(auto i : a){
+        int idx= lower_bound(b.begin(),b.end(),i)-b.begin();
+        ret+=idx;
+    }
+    return ret;
+}
+
+//dp : 메모리를 이용해서 시간복잡도 줄이기!
+//a,b의 idx를 가지고 합배열만들기
+//그때 a의 승리수 리턴
+void dfs(vector<int>& a, vector<int>& b, int depth, int sum1,int sum2){
+    if(depth==a.size()){
+        arrA.push_back(sum1);
+        arrB.push_back(sum2);
+        return ;
     }
     
     for(int i=0;i<6;++i){
-        if(vis[i]) continue;
-        vis[i]=1;
-        go(depth+1,val+dice[idxs[depth]][i],idxs);
-        vis[i]=0;
-    }
-}
-
-void go2(int depth, int val, vector<int> &idxs){
-    if(depth==n/2){
-        arrB.push_back(val);
-        return;
+        dfs(a,b,depth+1,sum1+dice[a[depth]][i],sum2+dice[b[depth]][i]);
     }
     
-    for(auto idx : idxs){
-        for(int i=0;i<6;++i){
-            go(depth+1,val+dice[idx][i],idxs);
-        }
-        
-    }
+    
 }
 
-vector<int> solution(vector<vector<int>> _dice) {
-    dice=_dice;
-    vector<int> answer;
-    n=dice.size();
-    vector<int> v;
-    for(int i=0;i<n/2;++i) v.push_back(0);
-    for(int i=0;i<n/2;++i) v.push_back(1);
-    
-    //1. A가 가져갈 주사위 선택하기(1)
-    //1 1 0 0
-    do{
-        vector<int> a,b;
-        for(int i=0;i<n;++i){
-            if(v[i])
-                a.push_back(i); //a가가져갈 주사위의 인덱스저장
-            else
-                b.push_back(i); //b가가져갈 주사위의 인덱스저장
+//현재까지 k 개뽑음
+void go(int k){
+    if(k==n/2){
+        vector<int> a,b; //a,b가 고른 주사위의 인덱스
+        for(int i=0;i<n;++i) s.insert(i);
+        for(int i=0;i<n/2;++i){
+            a.push_back(arr[i]);
+            s.erase(arr[i]);
+            //cout<<arr[i]<<" ";
         }
+        for(auto i : s){
+            b.push_back(i);
+        }
+        // cout<<"a: "; for(auto i : a) cout<<i<<" "; cout<<"\n";
+        // cout<<"b: "; for(auto i : b) cout<<i<<" ";
+        // cout<<"------------------\n";
         
         arrA.clear();
         arrB.clear();
-        go(0,0,a);
-        go2(0,0,b);
+        dfs(a,b,0,0,0);
         
-        sort(arrA.begin(),arrA.end());
-        sort(arrB.begin(),arrB.end());
-        
-        //for(auto i : arrA) cout<<i<<" ";
-        //cout<<"\n";
-        
-        int winA=0;
-        for(auto i : arrA){
-            winA+=(arrB.end()-lower_bound(arrB.begin(),arrB.end(),i));
-            //cout<<winA<<"\n";
-        }
-        if(winA>maxA){
+        //이분탐색
+        int cnt = go2(arrA,arrB);
+        if(cnt>=mx){ //a의 승률이 큰경우 발견
+            mx=cnt;
             answer.clear();
-            for(auto i : a){
-                answer.push_back(i+1);
+            for(int i : a){
+                answer.push_back(i+1); //1-idx로 만듬
             }
-            maxA=winA;
         }
-    }while(next_permutation(v.begin(),v.end()));
+        
+        
+        return;
+    }
     
     
-    //2. 가져간 주사위를 굴린 부분 계산
+    int st=0;
+    if(k!=0) st=arr[k-1];
+    for(int i=st;i<n;++i){
+        if(vis[i]) continue;
+        vis[i]=1;
+        arr[k]=i;
+        go(k+1);
+        vis[i]=0;
+    }
+}
+vector<int> solution(vector<vector<int>> _dice) {
+
+    dice=_dice;
+    n=dice.size();
+    for(int i=0;i<n;++i) s.insert(i);
+    go(0);
     return answer;
 }
