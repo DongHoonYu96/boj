b75f357cffd8d31e63cb65f8e32f0ec8766260df
DongHoonYu96 <50190387+DongHoonYu96@users.noreply.github.com>
2024-05-15 21:50:43 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 4 ms, Memory: 25460 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define endl "\n";
+
+using namespace std;
+typedef long long ll;
+
+
+ll n, lis[1000000 + 4], len, num;
+ll Index_Arr[1000000 + 4]; //index[3]=1 : 원본3번, lis 1번
+ll a[1000000 + 4]; //원본배열
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    memset(Index_Arr, -1, sizeof(Index_Arr));
+
+    //lis의 길이를 찾음
+    //index_arr에 해당값이 lis몇번인지 기록 
+    // ex:index_arr[3]=1  // 원본3번이 lis 1번임
+    //0-idx 사용
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        a[i] = num;
+
+        int idx = lower_bound(lis, lis + len, num) - lis;
+  
+        if (idx==len) len++; 
+        lis[idx] = num;
+
+        //lis 배열 찾기위한 로직 추가
+        Index_Arr[i] = idx;
+        
+    }
+    cout << len<<"\n";
+
+    /*for (int i = 0; i < n; ++i) {
+        cout << Index_Arr[i] << " ";
+    }cout << endl;*/
+
+    //index_Arr배열을 탐색하면서
+    vector<ll> v;
+    int find_idx = len - 1;
+    for (int i = n-1 ; i >= 0; --i) { //--i 역순탐색 => 3 2 4 에서 2가 자동 pass됨!
+        if (Index_Arr[i] == find_idx) {
+            find_idx--;
+            v.push_back(a[i]);
+        }
+    }
+    reverse(v.begin(), v.end());
+    for (auto i : v) {
+        cout << i << " ";
+    }   
+    return 0;
+}
+
+
