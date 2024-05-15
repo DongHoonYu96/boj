73544b238bf19a770b2abc1c805f32212f294e20
DongHoonYu96 <50190387+DongHoonYu96@users.noreply.github.com>
2024-05-15 21:26:55 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 240 ms, Memory: 37876 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -13,7 +13,7 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    memset(Index_Arr, -1, sizeof(Index_Arr));
+    //memset(Index_Arr, -1, sizeof(Index_Arr));
 
     cin >> n;
     for (int i = 0; i < n; i++) {
