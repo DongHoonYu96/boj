abd54eeb43e2d1d5d9a21328276358bf23f56054
DongHoonYu96 <50190387+DongHoonYu96@users.noreply.github.com>
2024-05-15 21:18:39 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 164 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -13,7 +13,7 @@ int main() {
         //1. num 이상인 idx를 찾음 -> 더 작은값이 들어오면, 작은값으로 교체함!
         //lis(현재의 증가수열길이) 중에서
         int idx = lower_bound(lis, lis + len, num) - lis;
-        if (lis[idx] == 0) len++; //처음보는값인경우, lis길이+1
+        if (idx==len) len++; //처음보는값인경우, lis길이+1
         lis[idx] = num; 
         /*for (int j = 0; j < n; j++) {
             printf("%d ", lis[j]);
