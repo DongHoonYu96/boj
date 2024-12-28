b9c12241c80343b43efa3742351fea1cb10ab50d
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2024-12-28 12:27:40 +0900

    [Gold IV] Title: 오큰수, Time: 200 ms, Memory: 34780 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,32 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-typedef long long int ll;
-int m, n, a[1000004], ret[1000004];   
-stack<int> s;
+typedef long long ll;
+
+ll n;
+vector<ll> arr;
+ll ans[1000000 + 4];
+stack<pair<ll, ll>> s; // idx, val
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(NULL); cout.tie(NULL);
+    cin.tie(0);
 
-    fill(&ret[0], &ret[1000004], -1);
     cin >> n ;
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> a[i];
-        while (s.size() && a[s.top()] < a[i])
-        {
-            ret[s.top()] = a[i];
+
+    for (int i = 0; i < n; ++i) {
+        ll tmp;
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+    
+    s.push({ 0,arr[0] });
+
+    for (int i = 1; i < n; ++i) {
+        ll cur = arr[i];
+        while (s.size() && s.top().second < cur) {
+            ans[s.top().first] = cur;
             s.pop();
         }
-        s.push(i);  //idx를 스택에 push하라.
+        s.push({ i,cur });
     }
 
-    for (int i = 0; i < n; ++i)
-    {
-        cout << ret[i] << " ";
+    while (s.size()) {
+        ll idx = s.top().first;
+        ans[idx] = -1;
+        s.pop();
     }
 
-
-
+    for (int i = 0; i < n; ++i) {
+        cout << ans[i] << " ";
+    }
+    
     return 0;
 }
