bc256423c0b671040d426ca930c845f5c0fe3f7b
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-01-25 12:02:31 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -2,26 +2,41 @@
 using namespace std;
 typedef long long ll;
 
-ll n, m,ret,temp;
-vector<ll> v;
+int n,m,ret;
+int arr[10000+4], sum[10000+4];
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
 
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i) {
-		cin >> temp;
-		v.push_back(temp);
-	}
-	v.push_back(0);
+   cin>>n>>m;
+   for(int i=0;i<n;++i) {
+      cin>>arr[i];
+   }
 
-	ll s = 0, e = 1, sum = v[0];
-	while (1) {
-		if (sum == m) ret++;
-		if (sum <= m) sum += v[e++];
-		if (sum > m)sum -= v[s++];
-		if (e == v.size()) break;
-	}
-	cout << ret;
+   int st=0; // sum에 포함
+   int en=1; //sum에 안포함 => while문 범위를 같은것까지 넣어야함.
+   int sum=arr[st];
+
+   while(en<=n) {
+      //감소하는 수 필요
+      if(sum==m) {
+         ret++;
+         sum-=arr[st];
+         st++;   
+      }
+      if(sum>m) {
+         sum-=arr[st];
+         st++; 
+      }
+      //증가하는 수 필요
+      if(sum<m) {
+         sum+=arr[en];
+         en++; 
+      }
+   }
+   
+   cout<<ret;
+
+   return 0;
 }
