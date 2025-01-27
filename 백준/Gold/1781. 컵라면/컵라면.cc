036e30e5e45fd696fa743edc31e75134d7e5fedd
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-01-27 19:47:03 +0900

    [Gold II] Title: 컵라면, Time: 68 ms, Memory: 6244 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -1,61 +1,36 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 typedef long long ll;
 
-class cmp {
-public:
-	bool operator () (pair<ll, ll> p1, pair<ll, ll> p2) {
-		if (p1.first == p2.first) {
-			return p1.second < p2.second;
-		}
-		return p1.first > p2.first;
-	}
-};
-priority_queue<pair<ll, ll>, vector<pair<ll, ll>>,cmp> pq;
-priority_queue<ll, vector<ll>, greater<int>> pqans;
+int n,ret;
+vector<pair<int,int>> v; // <데드라인, 컵라면 >
+priority_queue<int, vector<int>, greater<>> pq; //컵라면 최소힙
 
-ll n,ret;
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
 
+   cin>>n;
+   for (int i=0;i<n;++i) {
+      int a,b;
+      cin>>a>>b;
+      v.push_back({a,b});
+   }
+   sort(v.begin(),v.end());
+   
+   for(int i=0;i<n;++i) {
+      pq.push(v[i].second);
+      if(pq.size()>v[i].first) {
+         pq.pop();
+      }
+   }
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		ll a, b;
-		cin >> a >> b;
-		pq.push({ a,b });
-	}
-
-	/*while (pq.size()) {
-		cout << pq.top().first << " " << pq.top().second << "\n";
-		pq.pop();
-	}*/
-
-	int day = 1;
-	while (pq.size()) {
-		auto cur = pq.top(); pq.pop();
-		//데드라인중 최대값을 ans큐에 넣는다
-		if (day <= cur.first) {
-			pqans.push(cur.second);
-			day++;
-		}
-		else {
-			//같은데드라인중 더작은값은 자동으로패스됨
-			//이전데드라인(ans.top)을 선택하지않는것이 최적인경우
-			if (pqans.top() < cur.second) {
-				pqans.pop();
-				pqans.push(cur.second);
-			}
-		}
-	}
-
-	while (pqans.size()) {
-		ret += pqans.top();
-		pqans.pop();
-	}
-
-	cout << ret;
-	return 0;
+   while(pq.size()) {
+      ret+=pq.top();
+      pq.pop();
+   }
+   cout<<ret;
+   
+   
+   return 0;
 }
