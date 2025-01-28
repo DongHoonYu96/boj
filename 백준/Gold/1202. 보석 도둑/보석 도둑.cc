cba1fe6685e155007864f817392df701b769d9e3
DongHoonYu <50190387+DongHoonYu96@users.noreply.github.com>
2025-01-29 00:24:12 +0900

    [Gold II] Title: 보석 도둑, Time: 148 ms, Memory: 12388 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -2,45 +2,45 @@
 using namespace std;
 typedef long long ll;
 
-int n, k;
-ll ret; // int → long long
-vector<pair<int, int>> v;
-vector<int> bag;
-priority_queue<int> pq;
+ll n,k,ret;
+vector<pair<int,int>> v; // < 무게, 가격 >
+vector<int> bag; // 가방무게들
+priority_queue<int,vector<int>> pq; // 최대힙
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int main(){
+   ios_base::sync_with_stdio(0);
+   cin.tie(0);
 
-    cin >> n >> k;
-    for (int i = 0; i < n; ++i) {
-        int a, b;
-        cin >> a >> b;
-        v.push_back({a, b});
-    }
-    for (int i = 0; i < k; ++i) {
-        int a;
-        cin >> a;
-        bag.push_back(a);
-    }
+   cin>>n>>k;
+   for (int i=0;i<n;++i) {
+      int a,b;
+      cin>>a>>b;
+      v.push_back({a,b});
+   }
+   for (int i=0;i<k;++i) {
+      int a;
+      cin>>a;
+      bag.push_back(a);
+   }
+   // 보석 무게순 정렬
+   sort(v.begin(),v.end());
 
-    sort(v.begin(), v.end());
-    sort(bag.begin(), bag.end());
-
-    int j = 0;
-    for (int i = 0; i < k; ++i) {
-        // 현재 가방에 담을 수 있는 모든 보석 추가
-        while (j < n && bag[i] >= v[j].first) {
-            pq.push(v[j].second);
-            ++j;
-        }
-        // pq가 비어있지 않을 때만 처리
-        if (!pq.empty()) {
-            ret += pq.top();
-            pq.pop();
-        }
-        // if(j >= n) break; → 삭제!
-    }
-    cout << ret;
-    return 0;
+   //가방 오름차순정렬
+   sort(bag.begin(), bag.end());
+   
+   //각 가방에 대해
+   int j=0; // 보석을 가르키는 포인터
+   for(int i=0;i<k;++i) {
+      while(j<n && bag[i]>=v[j].first) { //가능한 보석들을 다 담음
+         pq.push(v[j].second);
+         ++j;
+      }
+      if(pq.size()) {
+         ret+=pq.top();
+         pq.pop();
+      }
+      // if(j>=n) break;
+   }
+   cout<<ret;
+   return 0;
 }
